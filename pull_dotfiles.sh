f0b3aa992e74a325a430052db206ac49cca1e239
Chaosbengel <contact@chaosbengel.de>
2025-09-01 18:58:48 +0200

    Update script

--- pull_dotfiles.sh
+++ pull_dotfiles.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
 # Just a helper script that collects my dotfiles from my system
-# and updates this repo
+# to update this repo
 
-cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+DOTFILES_DIR="dotfiles"
 
-cp "$HOME/.bashrc" bashrc
+DOTFILE_TGTLOC=(
+    "$HOME/.bashrc:bashrc"
+    )
+
+set -eu
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+[ -d "$DOTFILES_DIR" ] || mkdir -p "$DOTFILES_DIR"
+
+for item in "${DOTFILE_TGTLOG[@]}"; do
+    file="${item%%:*}"
+    tgt="${item##*:}"
+    dir=$(dirname "$tgt")
+    [ -d "$dir" ] || mkdir -p "$dir"
+    cp "$file" "$tgt"
+done
