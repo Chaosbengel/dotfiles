cab7cf38360c5e25f2aeaca7286242fd0a19e3ac
Chaosbengel <contact@chaosbengel.de>
2025-09-01 19:05:46 +0200

    Fix typo

--- pull_dotfiles.sh
+++ pull_dotfiles.sh
@@ -3,19 +3,19 @@
 # Just a helper script that collects my dotfiles from my system
 # to update this repo
 
+set -eu
+
 DOTFILES_DIR="dotfiles"
 
 DOTFILE_TGTLOC=(
     "$HOME/.bashrc:bashrc"
     )
 
-set -eu
-
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 [ -d "$DOTFILES_DIR" ] || mkdir -p "$DOTFILES_DIR"
 
-for item in "${DOTFILE_TGTLOG[@]}"; do
+for item in "${DOTFILE_TGTLOC[@]}"; do
     file="${item%%:*}"
     tgt="${item##*:}"
     dir=$(dirname "$tgt")
